f26ed1c8b5585334fe3f12ec53509f093ec021b9
baskarm <baskarmanickam@gmail.com>
2025-07-30 08:07:16 -0500

    🧹 Final cleanup: remove all source files before static deployment

--- docs/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Installed');
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(fetch(event.request));
-  
-});
